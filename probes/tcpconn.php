09131f1cf996b8b4c2c882854270d103213e482c
nanawel <nanawel@gmail.com>
2020-05-09 10:46:54 +0200

    tcpconn: Add netcat alternative method

--- probes/tcpconn.php
+++ probes/tcpconn.php
@@ -1,38 +1,85 @@
 <?php
 
-$probe = new Probe();
-$config = json_decode($probe->getConfig(), true);
+class TcpConn extends Probe {
+    const METHOD_NATIVE = 'native';
+    const METHOD_NETCAT = 'netcat';
 
-$results = [];
+    public function run()
+    {
+        $config = json_decode($this->getConfig(), true);
+        
+        $results = [];
+        
+        $startTime = microtime(true);
+        $hostIp = filter_var($config['host'], FILTER_VALIDATE_IP)
+            ? $config['host']
+            : gethostbyname($config['host']);
+        $results['resolve_time'] = microtime(true) - $startTime;
+        switch($config['connect_method'] ?? self::METHOD_NATIVE) {
+            case self::METHOD_NATIVE:
+                if (function_exists('socket_create')) {
+                    $results = array_merge($results, $this->runNative($config, $hostIp));
+                    break;
+                }
+                // No break, fallback to netcat
 
-$startTime = microtime(true);
-$hostIp = filter_var($config['host'], FILTER_VALIDATE_IP)
-    ? $config['host']
-    : gethostbyname($config['host']);
-$results['resolve_time'] = microtime(true) - $startTime;
+            case self::METHOD_NETCAT:
+                $results = array_merge($results, $this->runNetcat($config, $hostIp));
+        }
 
-if (false === ($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP))) {
-    exit(1);
+        $this->sendResults(
+            $results + [
+                // Default values
+                'connect_time' => -1,
+            ],
+            [
+                'probe_args' => sprintf('%s:%d', $config['host'], $config['port'])
+            ]
+        );
+    }
+
+    protected function runNative($config, $hostIp) {
+        $results = [];
+        
+        if (false === ($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP))) {
+            exit(1);
+        }
+        $startTime = microtime(true);
+        if (false === (socket_connect($socket, $hostIp, $config['port']))) {
+            exit(1);
+        }
+        $results['connect_time'] = microtime(true) - $startTime;
+        
+        if (!empty($config['banner'])) {
+            $startTime = microtime(true);
+            $banner = socket_read($socket, 255);
+            $results['banner_read_time'] = strpos($banner, $config['banner']) !== false
+                ? microtime(true) - $startTime
+                : -1;
+        }
+        
+        socket_close($socket);
+
+        return $results;
+    }
+
+    protected function runNetcat($config, $hostIp) {
+        $results = [];
+
+        $startTime = microtime(true);
+        $out = system(sprintf('nc -n -z %s %d', $hostIp, $config['port']), $rc);
+        if (false === $out || $rc  !== 0) {
+            return [];
+        }
+        $results['connect_time'] = microtime(true) - $startTime;
+        
+        if (!empty($config['banner'])) {
+            // Not supported
+            $results['banner_read_time'] = -1;
+        }
+
+        return $results;
+    }
 }
-$startTime = microtime(true);
-if (false === (socket_connect($socket, $hostIp, $config['port']))) {
-    exit(1);
-}
-$results['connect_time'] = microtime(true) - $startTime;
 
-if (!empty($config['banner'])) {
-    $startTime = microtime(true);
-    $banner = socket_read($socket, 255);
-    $results['banner_read_time'] = strpos($banner, $config['banner']) !== false
-        ? microtime(true) - $startTime
-        : -1;
-}
-
-socket_close($socket);
-
-$probe->sendResults(
-    $results,
-    [
-        'probe_args' => sprintf('%s:%d', $config['host'], $config['port'])
-    ]
-);
+(new TcpConn())->run();
