dbe46e6ff340345eebcf2d7bfde1d138d37534fe
nanawel <nanawel@gmail.com>
2020-05-09 18:35:49 +0200

    And again...

--- probes/python/loadavg.py
+++ probes/python/loadavg.py
@@ -4,7 +4,10 @@ import subprocess
 class LoadAvg(Probe):
     def run(self):
         load1, load5, load15 = os.getloadavg()
-        cpuCount = int(subprocess.check_output(['nproc']))
+        # Implementation that should maximize compatibility (even with Python 2.5)
+        p = subprocess.Popen(['nproc'], stdout=subprocess.PIPE)
+        p.wait()
+        cpuCount = int(p.communicate()[0])
         self.sendResults({'1m': load1, '5m': load5, '15m': load15, 'nproc': cpuCount})
 
 LoadAvg().run()
